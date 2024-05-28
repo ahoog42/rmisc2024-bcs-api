9677ca2f29118631cb06f8e8a1ebf789ecfe4274
Andrew Hoog <ahoog42@gmail.com>
2024-05-28 15:40:06 -0500

    better token checking, add verified token username to req

--- routes/api/v1/tenks.js
+++ routes/api/v1/tenks.js
@@ -6,8 +6,8 @@ const db = require('../../../lib/db');
 router.get('/', async function(req, res, next) {
   // first let's get the jwt token from the request
   let token = req.headers.authorization;
-  // if there is no token, return a 401 status code
-  if (!token) {
+  // if there is no token or not in format "BEARER <token>", return a 401 status code
+  if (!token || !token.split(' ')[1]) {
     return res.status(401).json({ error: 'Please provide a jwt token' });
   } else {
     // if there is a token, verify it
@@ -18,22 +18,24 @@ router.get('/', async function(req, res, next) {
         console.log('ERROR:', err);
         return res.status(401).json({ error: 'Invalid jwt token' });
       } else {
-          // if the token is valid, return the 5 most recent 10-Ks
-          const sql = `
-            SELECT filing.id, filing_id, company_id, url, items_json, company.name AS company_name, filed_at  
-            FROM filing 
-            LEFT JOIN company ON filing.company_id = company.id 
-            WHERE status = "10-K" 
-            ORDER BY filed_at DESC 
-            LIMIT 5
-          `;
-          try {
-            const data = await db.all(sql);
-            res.json(data);
-          } catch (error) {
-            console.log('ERROR:', error);
-            return res.status(500).json({ error: 'Unable to retrieve 10-Ks' });
-          }
+        // we have a valid user here so add to req.user
+        req.user = decoded.username;
+        // if the token is valid, return the 5 most recent 10-Ks
+        const sql = `
+          SELECT filing.id, filing_id, company_id, url, items_json, company.name AS company_name, filed_at  
+          FROM filing 
+          LEFT JOIN company ON filing.company_id = company.id 
+          WHERE status = "10-K" 
+          ORDER BY filed_at DESC 
+          LIMIT 5
+        `;
+        try {
+          const data = await db.all(sql);
+          res.json(data);
+        } catch (error) {
+          console.log('ERROR:', error);
+          return res.status(500).json({ error: 'Unable to retrieve 10-Ks' });
+        }
       }
     });
   }
